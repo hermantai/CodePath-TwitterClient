6c11c2675be007c1099d5beeb03be8b330065c0d
Herman Tai <htaihm@gmail.com>
2016-02-22 19:10:26 -0800

    Create a new fragment to replace the "include" in activity_timeline

---
+++ HermanTaisTwitterClient/app/src/main/java/com/codepath/apps/mysimpletweets/timeline/TimelineFragment.java
@@ -0,0 +1,26 @@
+package com.codepath.apps.mysimpletweets.timeline;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.mysimpletweets.R;
+
+public class TimelineFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweet_list, container, false);
+
+        return v;
+    }
+}
