70a129d557eea60e66d580508a2ed07b4380a738
Herman Tai <htaihm@gmail.com>
2016-02-28 17:52:40 -0800

    Implement MessageRecipientsFragment
    It shows a list of users who have direct messages to the authenticated
    user.

--- HermanTaisTwitterClient/app/src/main/java/com/codepath/apps/mysimpletweets/messages/MessageRecipientsFragment.java
+++ HermanTaisTwitterClient/app/src/main/java/com/codepath/apps/mysimpletweets/messages/MessageRecipientsFragment.java
@@ -1,19 +1,46 @@
 package com.codepath.apps.mysimpletweets.messages;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.codepath.apps.mysimpletweets.BuildConfig;
+import com.codepath.apps.mysimpletweets.Common;
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.SimpleTweetsApplication;
+import com.codepath.apps.mysimpletweets.helpers.ErrorHandling;
+import com.codepath.apps.mysimpletweets.helpers.LogUtil;
 import com.codepath.apps.mysimpletweets.helpers.NetworkUtil;
+import com.codepath.apps.mysimpletweets.helpers.StringUtil;
+import com.codepath.apps.mysimpletweets.models.Message;
+import com.codepath.apps.mysimpletweets.models.User;
 import com.codepath.apps.mysimpletweets.twitter.TwitterClient;
+import com.codepath.apps.mysimpletweets.widgets.SimpleProgressDialog;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,6 +48,10 @@ import butterknife.ButterKnife;
 public class MessageRecipientsFragment extends Fragment {
     @Bind(R.id.rvMessageRecipients) RecyclerView mRvMessageRecipients;
     private TwitterClient mClient;
+    private List<Message> mReceivedMessages;
+    private List<Message> mSentMessages;
+    private ProgressDialog mProgressDialog;
+    private MessageRecipientsAdapter mAdapter;
 
     public static MessageRecipientsFragment newInstance() {
         return new MessageRecipientsFragment();
@@ -30,6 +61,7 @@ public class MessageRecipientsFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mClient = SimpleTweetsApplication.getRestClient();
+        mAdapter = new MessageRecipientsAdapter();
     }
 
     @Nullable
@@ -41,6 +73,12 @@ public class MessageRecipientsFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_message_recipients, container, false);
         ButterKnife.bind(this, v);
 
+        LinearLayoutManager llManager = new LinearLayoutManager(getActivity());
+        llManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRvMessageRecipients.setLayoutManager(llManager);
+        mRvMessageRecipients.setAdapter(mAdapter);
+
         return v;
     }
 
@@ -51,6 +89,213 @@ public class MessageRecipientsFragment extends Fragment {
     }
 
     private void populate() {
+        mProgressDialog = SimpleProgressDialog.createProgressDialog(getActivity());
+        final Context context = getActivity();
+
+        mClient.getReceivedMessages(
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        if (BuildConfig.DEBUG) {
+                            LogUtil.d(
+                                    Common.INFO_TAG,
+                                    "fetch received messages : " + response.toString());
+                        }
+
+                        // Deserialize JSON
+                        // Create models
+                        // Note that response sorts tweets in descending IDs
+                        mReceivedMessages = Message.fromJsonArray(response);
+                        populateRecipients();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String
+                            responseString, Throwable throwable) {
+                        ErrorHandling.handleError(
+                                context,
+                                Common.INFO_TAG,
+                                "Error retrieving received messages: "
+                                        + throwable.getLocalizedMessage(),
+                                throwable);
+                        LogUtil.d(Common.INFO_TAG, responseString);
+
+                        mProgressDialog.dismiss();
+                        showSnackBarForNetworkError(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                populate();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(
+                            int statusCode,
+                            Header[] headers,
+                            Throwable throwable,
+                            JSONObject errorResponse) {
+                        ErrorHandling.handleError(
+                                context,
+                                Common.INFO_TAG,
+                                "Error retrieving received messages: "
+                                        + throwable.getLocalizedMessage(),
+                                throwable);
+                        LogUtil.d(
+                                Common.INFO_TAG,
+                                errorResponse == null ? "" : errorResponse.toString());
+
+                        mProgressDialog.dismiss();
+                        showSnackBarForNetworkError(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                populate();
+                            }
+                        });
+                    }
+                });
+
+        mClient.getSentMessages(
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        if (BuildConfig.DEBUG) {
+                            LogUtil.d(
+                                    Common.INFO_TAG,
+                                    "fetch sent messages : " + response.toString());
+                        }
+
+                        // Deserialize JSON
+                        // Create models
+                        // Note that response sorts tweets in descending IDs
+                        mSentMessages = Message.fromJsonArray(response);
+                        populateRecipients();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String
+                            responseString, Throwable throwable) {
+                        ErrorHandling.handleError(
+                                context,
+                                Common.INFO_TAG,
+                                "Error retrieving sent messages: "
+                                        + throwable.getLocalizedMessage(),
+                                throwable);
+                        LogUtil.d(Common.INFO_TAG, responseString);
+
+                        mProgressDialog.dismiss();
+                        showSnackBarForNetworkError(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                populate();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(
+                            int statusCode,
+                            Header[] headers,
+                            Throwable throwable,
+                            JSONObject errorResponse) {
+                        ErrorHandling.handleError(
+                                context,
+                                Common.INFO_TAG,
+                                "Error retrieving sent messages: "
+                                        + throwable.getLocalizedMessage(),
+                                throwable);
+                        LogUtil.d(
+                                Common.INFO_TAG,
+                                errorResponse == null ? "" : errorResponse.toString());
+
+                        mProgressDialog.dismiss();
+                        showSnackBarForNetworkError(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                populate();
+                            }
+                        });
+                    }
+                });
+    }
+
+    private void populateRecipients() {
+        if (mSentMessages != null && mReceivedMessages != null) {
+            Map<Long, User> users = new HashMap<>();
+            Map<Long, List<Message>> messagesForUsers = new HashMap<>();
+
+            for (Message m : mSentMessages) {
+                User recipient = m.getRecipient();
+                if (!users.containsKey(recipient.getUid())) {
+                    users.put(recipient.getUid(), recipient);
+                    List<Message> messages = new ArrayList<>();
+                    messages.add(m);
+                    messagesForUsers.put(recipient.getUid(), messages);
+                } else {
+                    List<Message> messages = messagesForUsers.get(recipient.getUid());
+                    messages.add(m);
+                }
+            }
+
+            for (Message m : mReceivedMessages) {
+                User sender = m.getSender();
+                if (!users.containsKey(sender.getUid())) {
+                    users.put(sender.getUid(), sender);
+                    List<Message> messages = new ArrayList<>();
+                    messages.add(m);
+                    messagesForUsers.put(sender.getUid(), messages);
+                } else {
+                    List<Message> messages = messagesForUsers.get(sender.getUid());
+                    messages.add(m);
+                }
+            }
+
+            // sort all messages with created at in descending order
+            for (List<Message> messages : messagesForUsers.values()) {
+                Collections.sort(messages, new Comparator<Message>() {
+                    @Override
+                    public int compare(Message lhs, Message rhs) {
+                        if (lhs.getCreatedAt()
+                                .before(rhs.getCreatedAt())) {
+                            return 1;
+                        } else if (lhs.getCreatedAt()
+                                .after(rhs.getCreatedAt())) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                });
+            }
+
+            List<MessageRecipient> messageRecipients = new ArrayList<>();
+
+            for (Map.Entry<Long, User> entry : users.entrySet()) {
+                User user = entry.getValue();
+                // The first message of the messages is the most recent message
+                List<Message> messages = messagesForUsers.get(entry.getKey());
+                Message m = messages.get(0);
+                messageRecipients.add(new MessageRecipient(user, m, messages));
+            }
+
+            // Sort MessageRecipients with the message's createdAt in descending order
+            Collections.sort(messageRecipients, new Comparator<MessageRecipient>() {
+                @Override
+                public int compare(MessageRecipient lhs, MessageRecipient rhs) {
+                    if (lhs.getMostRecentMessage().getCreatedAt()
+                            .before(rhs.getMostRecentMessage().getCreatedAt())) {
+                        return 1;
+                    } else if (lhs.getMostRecentMessage().getCreatedAt()
+                            .after(rhs.getMostRecentMessage().getCreatedAt())) {
+                        return -1;
+                    }
+                    return 0;
+                }
+            });
+
+            mAdapter.addAll(messageRecipients);
+
+            mProgressDialog.dismiss();
+        }
     }
 
     protected void showSnackBarForNetworkError(View.OnClickListener listener) {
@@ -62,6 +307,96 @@ public class MessageRecipientsFragment extends Fragment {
                     .setAction("Reload", listener)
                     .setActionTextColor(Color.YELLOW)
                     .show();
+        }
+    }
+
+    class MessageRecipientViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.ivMessageRecipientUserProfileImage)
+        ImageView mIvMessageRecipientUserProfileImage;
+
+        @Bind(R.id.tvMessageRecipientCreatedAt)
+        TextView mTvMessageRecipientCreatedAt;
+
+        @Bind(R.id.tvMessageRecipientMessage)
+        TextView mTvMessageRecipientMessage;
+
+        @Bind(R.id.tvMessageRecipientUserName)
+        TextView mTvMessageRecipientUserName;
+
+        private Activity mActivity;
+
+        public MessageRecipientViewHolder(View itemView, Activity activity) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            mActivity = activity;
+        }
+
+        private void bindMessageRecipient(MessageRecipient messageRecipient) {
+            Glide.with(mActivity)
+                    .load(messageRecipient.getUser().getProfileImageUrl())
+                    .into(mIvMessageRecipientUserProfileImage);
+            mTvMessageRecipientUserName.setText(messageRecipient.getUser().getName());
+            mTvMessageRecipientMessage.setText(messageRecipient.getMostRecentMessage().getText());
+            mTvMessageRecipientCreatedAt.setText(
+                    StringUtil.getRelativeTimeSpanString(
+                            messageRecipient.getMostRecentMessage().getCreatedAt()));
+        }
+    }
+
+    class MessageRecipientsAdapter extends RecyclerView.Adapter<MessageRecipientViewHolder> {
+        private List<MessageRecipient> mMessageRecipients = new ArrayList<>();
+
+        @Override
+        public MessageRecipientViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getActivity()).inflate(
+                    R.layout.item_message_recipient, parent, false);
+
+            return new MessageRecipientViewHolder(v, getActivity());
+        }
+
+        @Override
+        public void onBindViewHolder(MessageRecipientViewHolder holder, int position) {
+            holder.bindMessageRecipient(getItem(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mMessageRecipients.size();
+        }
+
+        private void addAll(List<MessageRecipient> users){
+            int oldLen = mMessageRecipients.size();
+            mMessageRecipients.addAll(users);
+            notifyItemRangeInserted(oldLen, users.size());
+        }
+
+        private MessageRecipient getItem(int position) {
+            return mMessageRecipients.get(position);
+        }
+    }
+
+    static class MessageRecipient {
+        private User mUser;
+        private Message mMostRecentMessage;
+        private List<Message> mAllMessages;
+
+        public MessageRecipient(User user, Message mostRecentMessage, List<Message> allMessages) {
+            mUser = user;
+            mMostRecentMessage = mostRecentMessage;
+            mAllMessages = allMessages;
+        }
+
+        public User getUser() {
+            return mUser;
+        }
+
+        public Message getMostRecentMessage() {
+            return mMostRecentMessage;
+        }
+
+        public List<Message> getAllMessages() {
+            return mAllMessages;
         }
     }
 }
--- HermanTaisTwitterClient/app/src/main/java/com/codepath/apps/mysimpletweets/models/Message.java
+++ HermanTaisTwitterClient/app/src/main/java/com/codepath/apps/mysimpletweets/models/Message.java
@@ -1,4 +1,59 @@
 package com.codepath.apps.mysimpletweets.models;
 
+import com.codepath.apps.mysimpletweets.Common;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Message {
+    private long mUid;
+    private User mSender;
+    private User mRecipient;
+    private Date mCreatedAt;
+    private String mText;
+
+    public long getUid() {
+        return mUid;
+    }
+
+    public User getSender() {
+        return mSender;
+    }
+
+    public User getRecipient() {
+        return mRecipient;
+    }
+
+    public Date getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public static ArrayList<Message> fromJsonArray(JSONArray jsonArray) {
+        ArrayList<Message> messages = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++ ) {
+            try {
+                messages.add(Message.fromJson(jsonArray.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+
+        return messages;
+    }
+
+    public static Message fromJson(JSONObject jsonObject) {
+        Gson gson = Common.getGson();
+        Message message = gson.fromJson(jsonObject.toString(), Message.class);
+        return message;
+    }
 }
